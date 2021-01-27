a885ff405991a415a0112cfca6f68e83bd30361a
David Ghazi <davidaghazi@gmail.com>
2021-01-26 22:32:59 -0500

    completed toggle between audiobook and e book

--- App.js
+++ App.js
@@ -2,16 +2,13 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 import MyTabs from './components/NavigationBar/MyTabs';
-
-
- import RootStack from './screens/RootStackScreen'
-
+import RootStack from './screens/RootStackScreen';
 
 const App = () => {
   return (
     <NavigationContainer>
       {/* <RootStack /> */}
-      <MyTabs/>
+      <MyTabs />
     </NavigationContainer>
   );
 };
--- components/EbookScreen/Ebook.js
+++ components/EbookScreen/Ebook.js
@@ -1,21 +1,82 @@
-import * as React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import React, {Component} from 'react';
+import {Text, StyleSheet, View, TouchableOpacity} from 'react-native';
 
-function EbookScreen({}) {
-  return (
-    <>
-      <Text style={styles.container}>Ebook/Audiobook Screen</Text>
-    </>
-  );
+class EbookScreen extends Component {
+  state = {
+    toggle: true,
+  };
+  _onPressEbook() {
+    const newState = false;
+    this.setState({toggle: newState});
+  }
+  _onPressAudio() {
+    const newState = true;
+    this.setState({toggle: newState});
+  }
+  render() {
+    const {toggle} = this.state;
+    const textColor = toggle ? 'white' : 'gray';
+    const textColor2 = toggle ? 'gray' : 'white';
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Books! Books! Books!</Text>
+        <View style={styles.innerContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => this._onPressAudio()}>
+            <Text
+              style={{
+                margin: 10,
+                color: 'black',
+                textAlign: 'center',
+                height: 30,
+                backgroundColor: textColor,
+              }}>
+              Ebook
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => this._onPressEbook()}>
+            <Text
+              style={{
+                margin: 10,
+                color: 'black',
+                textAlign: 'center',
+                height: 30,
+                backgroundColor: textColor2,
+              }}>
+              Audiobook
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+  },
+  header: {
+    margin: '15%',
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    margin: '5%',
+  },
+  button: {
+    flex: 1,
+    height: 50,
     justifyContent: 'center',
-    marginTop: '100%',
-    marginLeft: '30%',
+    backgroundColor: 'gray',
+  },
+  text: {
+    margin: 5,
+    color: 'black',
+    textAlign: 'center',
   },
 });
 

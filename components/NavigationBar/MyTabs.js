0ac059538890db63c2606c63e367fb5db7eb272b
David Ghazi <davidaghazi@gmail.com>
2021-01-31 23:00:03 -0500

    added icons for navigator

--- components/NavigationBar/MyTabs.js
+++ components/NavigationBar/MyTabs.js
@@ -6,11 +6,35 @@ import SearchScreen from '../SearchScreen/Search';
 import LibraryScreen from '../LibraryScreen/Library';
 import ProfileScreen from '../ProfileScreen/Profile';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home';
+          } else if (route.name === 'Search') {
+            iconName = focused ? 'search' : 'search';
+          } else if (route.name === 'Library') {
+            iconName = focused ? 'book' : 'book';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person';
+          }
+
+          // You can return any component that you like here!
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'blue',
+        inactiveTintColor: 'gray',
+      }}>
       <Tab.Screen name="Home" component={EbookScreen} />
       <Tab.Screen name="Search" component={SearchScreen} />
       <Tab.Screen name="Library" component={LibraryScreen} />

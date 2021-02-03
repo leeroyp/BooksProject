499b6e18325b930d660866e3aa475a320f1a53cd
David Ghazi <davidaghazi@gmail.com>
2021-02-03 00:04:59 -0500

    Dghazi/vector icons (#6)
    * added reusable image component
    
    * updating ebook screen
    
    * added icons for navigator

---
+++ components/EbookScreen/ImageDetail.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+const ImageDetail = (props) => {
+  return (
+    <View>
+      <Image style={styles.image} source={props.imageSource} />
+      <Text>{props.title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 250,
+    flexDirection: 'row',
+  },
+});
+
+export default ImageDetail;
